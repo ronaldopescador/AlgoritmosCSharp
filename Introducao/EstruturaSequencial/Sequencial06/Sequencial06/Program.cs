8563685a4322eda0168db6aadfbe9f5dd67d8a4b
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 21:36:24 -0300

    Commit inicial

---
+++ Introducao/EstruturaSequencial/Sequencial06/Sequencial06/Program.cs
@@ -0,0 +1,9 @@
+﻿// Leitura do peso do prato montado 
+Console.Write("Digite o peso (em quilos) do prato montado: ");
+double pesoPrato = Convert.ToDouble(Console.ReadLine());
+
+// Cálculo do valor total a pagar
+double totalPagar = pesoPrato * 12.00;
+
+// Exibição do valor total a pagar
+Console.WriteLine($"O valor a pagar é de {totalPagar:F2}.");
