b909361624716990a0186ad2b3ce4d50b3dbae19
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-11 22:23:45 -0300

    Exemplo03

---
+++ EstruturaDecisao/Exemplo03_TresNumeros_Encadeado/Exemplo03_TresNumeros_Encadeado/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Digite o primeiro número: ");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Digite o segundo número: ");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Digite o terceiro número: ");
+int numero3 = Convert.ToInt32(Console.ReadLine());
+if (numero1 == numero2 && numero1 == numero3)
+    Console.WriteLine("Todos os números são iguais.");
+else if (numero1 != numero2 && numero1 != numero3 && numero2 != numero3)
+    Console.WriteLine("Dois dos números são iguais.");
+else
+    Console.WriteLine("Nenhum dos números é igual.");
+
