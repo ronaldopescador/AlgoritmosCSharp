da1c19a5ca8f7cf7e03c64f658dddea3794153ec
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-11 23:34:45 -0300

    .

---
+++ EstruturaDecisao/Exercicio07_Soma/Exercicio07_Soma/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite o primeiro número: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int resultado = num1 * num2;
+
+Console.WriteLine("O resultado é: {0}", resultado);
+
---
+++ EstruturaDecisao/Exercicio08_Dobro/Exercicio08_Dobro/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Digite um número: ");
+int num = int.Parse(Console.ReadLine());
+num = num * 2;
+Console.WriteLine(num);
+
