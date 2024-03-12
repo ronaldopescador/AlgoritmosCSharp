08bc8d9f36dd63abb13bc6b1f0efcd1ae2242f38
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-11 21:55:58 -0300

    Maior número

---
+++ EstruturaDecisao/Exercicio03_TresNumeros/Exercicio03_TresNumeros/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Digite o primeiro número: ");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Digite o segundo número: ");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Digite o terceiro número: ");
+int numero3 = Convert.ToInt32(Console.ReadLine());
+if (numero1 == numero2 && numero1 == numero3)
+    Console.WriteLine("Todos os números são iguais.");
+if ((numero1 == numero2 && numero1 != numero3) ||
+    (numero1 == numero3 && numero1 != numero2) ||
+    (numero2 == numero3 && numero2 != numero1))
+    Console.WriteLine("Dois dos números são iguais.");
+if (numero1 != numero2 && numero1 != numero3 &&
+    numero2 != numero3)
+    Console.WriteLine("Nenhum dos números é igual.");
+
---
+++ EstruturaDecisao/Exercicio05_MaiorNumero/Exercicio05_MaiorNumero/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Digite o primeiro número: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("O maior número é: {0} ", num1);
+}
+else
+{
+    Console.WriteLine("O maior número é: {0} ", num2);
+}
+
