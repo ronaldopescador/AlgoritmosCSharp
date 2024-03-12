b995b55995ca6f4668a06e0d303c32349cb73e5b
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-12 00:05:17 -0300

    .

---
+++ EstruturaDecisao/Exercicio09_TresNumerosReais/Exercicio09_TresNumerosReais/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Digite o primeiro número: ");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o terceiro número: ");
+double num3 = double.Parse(Console.ReadLine());
+
+double resultado = num1 + num2 * num3 - num2;
+
+Console.WriteLine($"O resultado é: {resultado}");
+
---
+++ EstruturaDecisao/Exercicio10_Desconto/Exercicio10_Desconto/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Digite um valor: ");
+double valor = double.Parse(Console.ReadLine());
+double fator;
+if (valor >= 100)
+{
+    fator = 0.1; 
+}
+else
+{
+    fator = 0.05; 
+}
+double resultado = valor - valor * fator;
+Console.WriteLine($"O resultado é: " + resultado);
+
