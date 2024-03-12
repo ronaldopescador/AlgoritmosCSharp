ffa161c770603b19496b8d3f4997124ebff4bb53
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-11 23:03:00 -0300

    .

---
+++ EstruturaDecisao/Exercicio06_Maior3/Exercicio06_Maior3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Digite o primeiro número: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o terceiro número: ");
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine("O maior número é: {0} ", num1);
+    }
+    else
+    {
+        Console.WriteLine("O maior número é: {0} ", ); num3;
+    }
+}
+else
+{
+    if (num2 > num3)
+    {
+        Console.WriteLine("O maior número é: {0} ", num2);
+    }
+    else
+    {
+        Console.WriteLine("O maior número é: {0} ", num3);
+    }
+}
