0dea29614b622e7743063a79e8a6eccea30690c1
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-12 00:10:52 -0300

    .

---
+++ EstruturaDecisao/Exercicio11_FaxaEtaria/Exercicio11_FaxaEtaria/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Digite sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade < 15)
+{
+    Console.WriteLine("Você é uma criança!");
+}
+else if (idade <= 29)
+{
+    Console.WriteLine("Você é um jovem!");
+}
+else
+{
+    Console.WriteLine("Você é um adulto!");
+}
