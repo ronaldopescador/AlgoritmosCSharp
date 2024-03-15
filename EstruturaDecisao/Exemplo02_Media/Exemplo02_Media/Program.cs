c3831f2522a898dd0decc0df3cc9ae77c41a9e3f
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-14 21:22:13 -0300

    .

--- EstruturaDecisao/Exemplo02_Media/Exemplo02_Media/Program.cs
+++ EstruturaDecisao/Exemplo02_Media/Exemplo02_Media/Program.cs
@@ -4,16 +4,16 @@ double nota1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Digite a segunda nota: ");
 double nota2 = Convert.ToDouble(Console.ReadLine());
 
-double media = (nota1 + nota2) / 2;
+const int QUANTIDADE_NOTAS = 2;
+
+double media = (nota1 + nota2) / QUANTIDADE_NOTAS;
 Console.WriteLine("Média {0:F2}: ", media);
 
-const int MEDIA_APROVADO = 6;
+const double MEDIA_APROVADO = 6;
 
 if (media >= MEDIA_APROVADO)
-{
     Console.WriteLine("Aluno aprovado!");
-}
 else
-{
     Console.WriteLine("Aluno reprovado!");
-}
+
+
