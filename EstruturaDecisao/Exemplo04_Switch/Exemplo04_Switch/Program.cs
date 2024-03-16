ecd34c9eb557622ac4b78e28b8a20466e7dea33a
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-16 19:25:16 -0300

    .

---
+++ EstruturaDecisao/Exemplo04_Switch/Exemplo04_Switch/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Digite o número do dia da semana:");
+int diaDaSemana = Convert.ToInt32(Console.ReadLine());
+switch (diaDaSemana)
+{
+    case 1:
+        Console.WriteLine("Segunda-feira");
+        break;
+    case 2:
+        Console.WriteLine("Terça-feira");
+        break;
+    case 3:
+        Console.WriteLine("Quarta-feira");
+        break;
+    // ...
+    default:
+        Console.WriteLine("Dia inválido");
+        break;
+}
+
