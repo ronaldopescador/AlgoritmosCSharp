602b63c7ccc8ab13b583af1b5daf8df6c3852f68
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-24 00:29:40 -0300

    .

---
+++ EstruturaRepeticao/Exemplo01_Numeros/Exemplo01_Numeros/Program.cs
@@ -0,0 +1,7 @@
+﻿//Escrever os números de 1 a 5;
+int num = 1;
+while (num <= 5)
+{
+    Console.Write(num);
+    num++;
+}
---
+++ EstruturaRepeticao/Exemplo02_Somar/Exemplo02_Somar/Program.cs
@@ -0,0 +1,13 @@
+﻿//Solicitar e somar uma quantidade de valores
+Console.WriteLine("Quantidade de valores a serem somados:");
+int quantidade = Convert.ToInt32(Console.ReadLine());
+int contador = 1;
+double soma = 0;
+while (contador <= quantidade)
+{
+    Console.WriteLine($"Digite o valor {contador}:");
+    double valor = Convert.ToDouble(Console.ReadLine());
+    soma += valor;
+    contador++;
+}
+Console.WriteLine($"A soma dos valores é: {soma}");
